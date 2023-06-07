731e635994cc8317d4c94f8632c13b1f6a418892
shm_usuf <shmusuf@gmail.com>
2023-06-07 20:56:47 +0600

    basic setup done

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//express app
+const app = express();
+
+//middlewares
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "welcome to our tormax server" });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`server is running on port:${port}`);
+});
